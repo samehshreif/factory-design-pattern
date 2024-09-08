0b644408702cd7f07271de5f8c5b3895326591fc
Eng Sameh <eng_sameh_86@outlook.com>
2024-09-08 22:13:01 +0400

    8920124

---
+++ factory-design-pattern-method/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace factory_design_pattern_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
